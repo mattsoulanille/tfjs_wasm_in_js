262ae98e40d13d5939d6eff71dbc03302b44ce88
Matthew Soulanille <msoulanille@google.com>
2023-05-25 11:25:56 -0700

    Load WASM from a local blob

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import * as tf from '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-core/dist/public/chained_ops/register_all_chained_ops';
 import '@tensorflow/tfjs-backend-wasm';
-import { setWasmPaths } from '@tensorflow/tfjs-backend-wasm';
+import {setWasmPaths} from '@tensorflow/tfjs-backend-wasm';
+import wasm from '@tensorflow/tfjs-backend-wasm/dist/tfjs-backend-wasm.wasm';
+import wasmSimd from '@tensorflow/tfjs-backend-wasm/dist/tfjs-backend-wasm-simd.wasm';
+import wasmThreaded from '@tensorflow/tfjs-backend-wasm/dist/tfjs-backend-wasm-threaded-simd.wasm';
+
+// TODO(mattSoulanille): tfjs-backend-wasm should probably support passing
+// the wasm data directly instead of needing to load it from a URL.
+function makeUrl(s) {
+  const wasmBuffer = Buffer.from(s, 'base64');
+  const wasmBlob = new Blob([wasmBuffer]);
+  const wasmUrl = URL.createObjectURL(wasmBlob);
+  return wasmUrl;
+}
 
 async function main() {
-  setWasmPaths('node_modules/@tensorflow/tfjs-backend-wasm/dist/');
+  // TODO(mattSoulanille): You shouldn't have to set all the wasm paths
+  // if you know which one you want to use.
+  setWasmPaths({
+    'tfjs-backend-wasm.wasm': makeUrl(wasm),
+    'tfjs-backend-wasm-simd.wasm': makeUrl(wasmSimd),
+    'tfjs-backend-wasm-threaded-simd.wasm': makeUrl(wasmThreaded),
+  }, /* use platform fetch */ true);
+
   await tf.setBackend('wasm');
   tf.tensor1d([1,2,3]).add(tf.tensor1d([4,5,6])).print();
 }
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,8 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import {visualizer} from 'rollup-plugin-visualizer';
+//import {wasm} from '@rollup/plugin-wasm';
+import {base64} from 'rollup-plugin-base64';
 
 export default {
   input: 'index.js',
@@ -18,6 +20,12 @@ export default {
         'perf_hooks',
         'os',
       ],
+    }),
+    // wasm({
+    //   maxFileSize: 1e7, // 10mb
+    // }),
+    base64({
+      include: '**/*.wasm'//'node_modules/@tensorflow/tfjs-backend-wasm/src/*.wasm',
     }),
     visualizer({
       sourcemap: true,
